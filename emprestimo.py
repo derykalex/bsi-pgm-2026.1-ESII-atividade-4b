af53e470fc2ffd3c2b36ff18ac9f95a17e71dd45
derykalex <derykalex@gmail.com>
2026-05-09 14:26:49 -0300

    Adiciona model Emprestimo
    Cria dataclass de empréstimo

---
+++ emprestimo.py
@@ -0,0 +1,13 @@
+# Emprestimo: representar contrato de empréstimo.
+from dataclasses import dataclass
+from datetime import date
+
+@dataclass
+class Emprestimo:
+    id: int
+    equipamento_id: int
+    nome_usuario: str
+    email_usuario: str
+    data_emprestimo: date
+    data_devolucao: date
+    devolvido: bool = False
