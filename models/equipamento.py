93f8cfac081bd912d54b6bf40ea03e85c51a5ca3
derykalex <derykalex@gmail.com>
2026-05-09 14:33:58 -0300

    Delete models directory

--- models/equipamento.py
+++
@@ -1,9 +0,0 @@
-# Equipamento: representar dados de equipamento.
-from dataclasses import dataclass
-
-@dataclass
-class Equipamento:
-    id: int
-    nome: str
-    tipo: str
-    disponivel: bool = True
