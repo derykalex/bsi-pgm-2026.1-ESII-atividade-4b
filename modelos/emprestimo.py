d15fcfed223c6d78fa71f6a45aa67d57bcf952e6
derykalex <derykalex@gmail.com>
2026-05-14 19:31:50 -0300

    Corrige sintaxe Python em emprestimo.py

--- modelos/emprestimo.py
+++ modelos/emprestimo.py
@@ -1,13 +1,15 @@
-# Emprestimo: contrato de empréstimo.
+# Emprestimo: contrato de empréstimo
+
 from dataclasses import dataclass
 from datetime import date
+
 
 @dataclass
 class Emprestimo:
     id: int
     equipamento_id: int
-    nome_usuario: str
-    email_usuario: str
+    usuario_nome: str
+    usuario_email: str
     data_emprestimo: date
     data_devolucao: date
     devolvido: bool = False
