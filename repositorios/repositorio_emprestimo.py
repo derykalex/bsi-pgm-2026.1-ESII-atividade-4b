ff836644b4a63b941dd0176f93a65149e36d3a63
derykalex <derykalex@gmail.com>
2026-05-31 07:51:38 -0300

    refactor: faz RepositorioEmprestimo herdar de IRepositorioEmprestimo

--- repositorios/repositorio_emprestimo.py
+++ repositorios/repositorio_emprestimo.py
@@ -1,9 +1,10 @@
 # RepositorioEmprestimo: persistência em memória
-
+from repositorios.interfaces import IRepositorioEmprestimo
 from modelos.equipamento import Notebook, Projetor, Tablet
+from datetime import date
 
 
-class RepositorioEmprestimo:
+class RepositorioEmprestimo(IRepositorioEmprestimo):
 
     def __init__(self):
         # Base inicial simulada
@@ -12,54 +13,48 @@ class RepositorioEmprestimo:
             Projetor(2, "Projetor Epson", "projetor"),
             Tablet(3, "Tablet Samsung", "tablet")
         ]
-
         self.emprestimos = []
 
-    # Buscar equipamento por ID
-    def buscar_equipamento(self, equip_id: int):
+    def buscar_equipamento(self, equip_id):
         for equipamento in self.equipamentos:
             if equipamento.id == equip_id:
                 return equipamento
         return None
 
-    # Salvar novo empréstimo
     def salvar_emprestimo(self, emprestimo):
         self.emprestimos.append(emprestimo)
 
-    # Tornar equipamento indisponível
-    def marcar_indisponivel(self, equip_id: int):
-        equipamento = self.buscar_equipamento(equip_id)
-
-        if equipamento:
-            equipamento.disponivel = False
-
-    # Buscar empréstimo por ID
-    def buscar_emprestimo(self, emprestimo_id: int):
+    def buscar_emprestimo(self, emprestimo_id):
         for emprestimo in self.emprestimos:
             if emprestimo.id == emprestimo_id:
                 return emprestimo
         return None
 
-    # Registrar devolução
-    def marcar_devolvido(self, emprestimo_id: int):
-        emprestimo = self.buscar_emprestimo(emprestimo_id)
-
-        if emprestimo:
-            emprestimo.devolvido = True
-
-    # Tornar equipamento disponível novamente
-    def marcar_disponivel(self, equip_id: int):
+    def marcar_indisponivel(self, equip_id):
         equipamento = self.buscar_equipamento(equip_id)
+        if equipamento:
+            equipamento.disponivel = False
 
+    def marcar_disponivel(self, equip_id):
+        equipamento = self.buscar_equipamento(equip_id)
         if equipamento:
             equipamento.disponivel = True
 
-    # Buscar empréstimos atrasados
+    def marcar_devolvido(self, emprestimo_id):
+        emprestimo = self.buscar_emprestimo(emprestimo_id)
+        if emprestimo:
+            emprestimo.devolvido = True
+
     def buscar_emprestimos_atrasados(self, hoje):
         atrasados = []
-
         for emprestimo in self.emprestimos:
             if not emprestimo.devolvido and emprestimo.data_devolucao < hoje:
                 atrasados.append(emprestimo)
-
         return atrasados
+
+    # Métodos adicionais da interface
+    def listar_em_atraso(self):
+        return self.buscar_emprestimos_atrasados(date.today())
+
+    def proximo_id_emprestimo(self):
+        return len(self.emprestimos) + 1
