3819f19b353b4ccb541db87e1a61b9fbacae4cba
derykalex <derykalex@gmail.com>
2026-05-31 08:00:05 -0300

    refactor: faz Notificador herdar de INotificador

--- servicos/notificador.py
+++ servicos/notificador.py
@@ -1,35 +1,24 @@
 # Notificador: responsável pela comunicação com o usuário.
+from servicos.interfaces import INotificador
 
 
-class Notificador:
+class Notificador(INotificador):
 
     # Notificação de registro de empréstimo
-    def notificar_emprestimo(
-        self,
-        email: str,
-        data_devolucao
-    ):
+    def notificar_emprestimo(self, email: str, data_devolucao):
         print(
             f"[EMAIL] Empréstimo registrado para {email}. "
             f"Data prevista para devolução: {data_devolucao}"
         )
 
     # Notificação de devolução concluída
-    def notificar_devolucao(
-        self,
-        email: str
-    ):
+    def notificar_devolucao(self, email: str, multa: float = 0.0):
         print(
             f"[EMAIL] Devolução registrada com sucesso para {email}"
         )
 
     # Notifica atraso com multa
-    def notificar_atraso(
-        self,
-        email: str,
-        multa: float
-    ):
+    def notificar_atraso(self, email: str):
         print(
-            f"[EMAIL] Atenção {email}: empréstimo em atraso. "
-            f"Multa acumulada: R${multa:.2f}"
+            f"[EMAIL] Atenção {email}: empréstimo em atraso."
         )
