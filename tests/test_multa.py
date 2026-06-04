d7d246ba2183ccc32c490cd69d4863dda8ef224f
derykalex <derykalex@gmail.com>
2026-06-04 11:49:54 -0300

    feat: multa zero quando sem atraso (ciclo 1)

--- tests/test_multa.py
+++ tests/test_multa.py
@@ -1,38 +1,5 @@
-from modelos.equipamento import Notebook
-from modelos.equipamento import Projetor
-from modelos.equipamento import Tablet
+from multa import calcular_multa_com_carencia
 
-
-def test_calcular_multa_notebook():
-    notebook = Notebook(1, "Dell", "notebook")
-    assert notebook.calcular_multa(3) == 30.0
-
-
-def test_calcular_multa_projetor():
-    projetor = Projetor(2, "Epson", "projetor")
-    assert projetor.calcular_multa(2) == 30.0
-
-
-def test_calcular_multa_tablet():
-    tablet = Tablet(3, "Samsung", "tablet")
-    assert tablet.calcular_multa(5) == 40.0
-
-
-def test_multa_notebook_sem_atraso():
-    notebook = Notebook(1, "Dell", "notebook")
-    assert notebook.calcular_multa(0) == 0
-
-
-def test_multa_projetor_sem_atraso():
-    projetor = Projetor(2, "Epson", "projetor")
-    assert projetor.calcular_multa(0) == 0
-
-
-def test_multa_tablet_sem_atraso():
-    tablet = Tablet(3, "Samsung", "tablet")
-    assert tablet.calcular_multa(0) == 0
-
-
-def test_multa_notebook_um_dia():
-    notebook = Notebook(1, "Dell", "notebook")
-    assert notebook.calcular_multa(1) > 0
+def test_atraso_zero_nao_gera_multa():
+    multa = calcular_multa_com_carencia(dias_atraso=0, valor_dia=10.0, carencia=2)
+    assert multa == 0.0
