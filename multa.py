f6a063856e6d1c7d50abe29f471d5b7a2d24e339
derykalex <derykalex@gmail.com>
2026-06-04 11:53:08 -0300

    feat: cobra dias alem da carencia (ciclo 2)

--- multa.py
+++ multa.py
@@ -1,2 +1,4 @@
 def calcular_multa_com_carencia(dias_atraso, valor_dia, carencia):
-    return 0.0  # fake it - ciclo 1
+    if dias_atraso <= carencia:
+        return 0.0
+    return (dias_atraso - carencia) * valor_dia
