7104df81d59fefd19d711c88c9fd12058fa95151
derykalex <derykalex@gmail.com>
2026-06-13 19:31:25 -0300

    Remove unnecessary line from __init__.py

--- repositorios/__init__.py
+++ repositorios/__init__.py
@@ -1,3 +1,1 @@
 # Pacote de persistência e acesso a dados.
-# Após criar os equipamentos
-self.equipamentos[0].definir_estrategia_multa(MultaComCarencia(valor_por_dia=10.0, carencia_dias=1))  # Notebook
