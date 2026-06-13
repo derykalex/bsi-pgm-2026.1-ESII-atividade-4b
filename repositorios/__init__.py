027c8e4e2024ccd9e2e16bbde74f600b76afc3d5
derykalex <derykalex@gmail.com>
2026-06-13 18:44:16 -0300

    Update __init__.py

--- repositorios/__init__.py
+++ repositorios/__init__.py
@@ -1,1 +1,3 @@
 # Pacote de persistência e acesso a dados.
+# Após criar os equipamentos
+self.equipamentos[0].definir_estrategia_multa(MultaComCarencia(valor_por_dia=10.0, carencia_dias=1))  # Notebook
