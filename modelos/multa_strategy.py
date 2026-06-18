da1cb9164de0eeb05c8cedf9dc8d43feb1dd7af7
derykalex <derykalex@gmail.com>
2026-06-17 22:05:28 -0300

    feat: cria hierarquia MultaStrategy

---
+++ modelos/multa_strategy.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class MultaStrategy(ABC):
+    @abstractmethod
+    def calcular(self, dias_atraso: int) -> float: ...
+
+class MultaPorDia(MultaStrategy):
+    def __init__(self, valor_dia: float):
+        self.valor_dia = valor_dia
+
+    def calcular(self, dias_atraso: int) -> float:
+        return max(0.0, dias_atraso * self.valor_dia)
