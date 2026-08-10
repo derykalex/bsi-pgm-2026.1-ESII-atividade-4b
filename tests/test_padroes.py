3ddda49e55a792b50c2012d1ac729a33a7b71162
derykalex <derykalex@gmail.com>
2026-08-10 12:32:46 -0300

    test: corrige testes do notificador

--- tests/test_padroes.py
+++ tests/test_padroes.py
@@ -56,7 +56,7 @@ def test_multa_com_carencia_com_atraso():
 
 
 def test_notificador_emprestimo(capsys):
-    notificador = Notificador()
+    notificador = Notificador.__new__(Notificador)
 
     evento = Evento(
         tipo="emprestimo",
@@ -73,7 +73,7 @@ def test_notificador_emprestimo(capsys):
 
 
 def test_notificador_devolucao(capsys):
-    notificador = Notificador()
+    notificador = Notificador.__new__(Notificador)
 
     evento = Evento(
         tipo="devolucao",
@@ -89,7 +89,7 @@ def test_notificador_devolucao(capsys):
 
 
 def test_notificador_atraso(capsys):
-    notificador = Notificador()
+    notificador = Notificador.__new__(Notificador)
 
     evento = Evento(
         tipo="atraso",
